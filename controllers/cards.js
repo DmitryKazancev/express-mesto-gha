e1281e13bf365e2ec373a39963c704780932a66d
Dmitrii Kazantsev <dmitrykaz@mail.ru>
2023-08-25 21:29:08 +0500

    fix: Card controller for likes

--- controllers/cards.js
+++ controllers/cards.js
@@ -41,7 +41,7 @@ module.exports.deleteCard = (req, res) => {
 };
 
 module.exports.likeCard = (req, res) => {
-  if (req.params.userId.length === 24) {
+  if (req.params.cardId.length === 24) {
     Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
       .populate(['owner', 'likes'])
       .then((card) => {
@@ -58,7 +58,7 @@ module.exports.likeCard = (req, res) => {
 };
 
 module.exports.dislikeCard = (req, res) => {
-  if (req.params.userId.length === 24) {
+  if (req.params.cardId.length === 24) {
     Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
       .populate(['owner', 'likes'])
       .then((card) => {
